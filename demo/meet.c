494581485167bb2eef898f388866ac35ec5d0126
Bijeeta Pal <bp397@cornell.edu>
2019-11-15 13:51:02 -0500

    bug fix in demo

--- demo/meet.c
+++ demo/meet.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 
-greeting( char* temp1, char* temp2 )
+greeting( char* temp1)
 {
 	char name[400];
 	memset(name, 0, 400);
-	strcpy(name, temp2);
-	printf( "Hi %s %s\n", temp1, name );
+	strcpy(name, temp1);
+	printf( "Hi %s\n", name );
 }
 
 
 int main(int argc, char* argv[] ) 
 {
-	greeting( argv[1], argv[2] );
-	printf( "Bye %s %s\n", argv[1], argv[2] );
+	greeting( argv[1] );
+	printf( "Bye \n" );
 }
